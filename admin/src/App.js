22e8e01cc678291445a1df8803702d08b9124406
walker <walker.wu@tctchina.com.cn>
2020-07-14 11:16:20 +0800

    创建后台系统项目

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "antd";
+import "./less/app.css"
+const App = () => {
+    return (
+        <>
+            你好
+            <Button>你</Button>
+            <span className="redfont">123</span>
+        </>
+    )
+}
+export default App;
